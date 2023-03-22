859cef24b062a6eeba1d393442cb6b34ec3c5ca2
Pedrolmf9 <pedroluizmf@gmail.com>
2023-03-22 00:17:10 -0300

    Add game function to script

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function getComputerChoice(){
     return choice;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection){
+    let roundWinner = "";
     playerSelection = playerSelection.toLowerCase();
     if (computerSelection == playerSelection) {
         console.log("It's a tie!");
@@ -16,14 +17,41 @@ function playRound(playerSelection, computerSelection) {
     (computerSelection == "paper" && playerSelection == "rock")
     ){
         console.log("Computer wins!");
+        roundWinner = "Computer";
+        return roundWinner
     }
     else {
         console.log("YAY! You win!");
+        roundWinner = "Player";
+        return roundWinner
     }
-
-
 }
 
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+function game(){
+    let playerWins = 0;
+    let computerWins = 0;    
+    
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Choose rock, paper or scissors:");
+        const computerSelection = getComputerChoice();
+        match = playRound(playerSelection, computerSelection);
+        if (match == "Computer"){
+            computerWins++;
+        }
+        else if (match == "Player"){
+            playerWins++;
+        }
+     }
+     if (computerWins > playerWins){
+        console.log("Computer is the final Winner!!");
+     }
+     else if (playerWins > computerWins){
+        console.log("You are the final Winner!!");
+     }
+     else{
+        console.log("Well,it's a tie...")
+     }
+}
+
+
+game();
